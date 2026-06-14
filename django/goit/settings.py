35d22690c8808db7209fecbcefd7a0c0359558f9
Viacheslav Dmytriiev <v.dmytriiev@sdventures.com>
2026-06-14 19:31:38 +0200

    Fixed issues

--- django/goit/settings.py
+++ django/goit/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%1&isz7zcrhngb#!#qi)6q5lkhojs$ez40*usxjnaao-en@wd(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
